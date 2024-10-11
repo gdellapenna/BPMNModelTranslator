0ece2840ec9edc169249e81a3122d03fa30b0376
Giuseppe Della Penna <giuseppe.dellapenna@univaq.it>
2024-10-11 11:10:57 +0200

    Updated compiler version

--- output.java
+++ output.java
@@ -1,4 +1,3 @@
-
 class TypeUtils {
 
     public static Double tonumber(Object o) {
@@ -29,84 +28,47 @@ class TypeUtils {
     }
 }
 
-class ProcessUtils {
-
-    public static void NoDefaultError() {
-        System.exit(9999);
+   class ProcessUtils {
+        public static void NoDefaultError() { System.exit(9999); }
+        public static void signal(String s) {  }
+        public static void wait(String... s) {  }
     }
 
-    public static void signal(String s) {
-    }
-
-    public static void wait(String... s) {
-    }
+ class bpmn_process_Process_1njo00q { ; public void flow_StartEvent_1() {//start event: null;
 }
 
-class bpmn_process_Process_1njo00q {
-
-    ; public void flow_StartEvent_1() {//start event: null;
-    }
-
-    public void flow_Task1() {
-        task_generic_Task1();
-        ProcessUtils.signal(ID DEL NODO ENTRANTE
-    
-
-    
-
-    );
+public void flow_Task1() {task_generic_Task1();
+ProcessUtils.signal(ID DEL NODO ENTRANTE);
 }
 
-public void flow_Task_4() {
-        task_generic_Task_4();
-        ProcessUtils.signal(ID DEL NODO ENTRANTE
-    
-
-    
-
-    );
+public void flow_Task_4() {task_generic_Task_4();
+ProcessUtils.signal(ID DEL NODO ENTRANTE);
 }
 
-public void flow_Task_5() {
-        task_generic_Task_5();
-        ProcessUtils.signal(ID DEL NODO ENTRANTE
-    
-
-    
-
-    );
+public void flow_Task_5() {task_generic_Task_5();
+ProcessUtils.signal(ID DEL NODO ENTRANTE);
 }
 
-public void flow_Task3() {
-        task_generic_Task3();
-    }
-
-    public void flow_Task2() {
-        task_generic_Task2();
-        if (pippo().equals(1.0)) {
-            flow_Task_4();
-        } else {
-            flow_Task_5();
-        };
-    }
-
-    public void task_generic_Task_4() {
-        System.out.println("task_generic_Task 4");
-    }
-
-    public void task_generic_Task_5() {
-        System.out.println("task_generic_Task 5");
-    }
-
-    public void task_generic_Task3() {
-        System.out.println("task_generic_Task3");
-    }
-
-    public void task_generic_Task2() {
-        System.out.println("task_generic_Task2");
-    }
-
-    public void task_generic_Task1() {
-        System.out.println("task_generic_Task1");
-    }
+public void flow_Task3() {task_generic_Task3();
 }
+
+public void flow_Task2() {task_generic_Task2();
+if (pippo().equals(1.0)){flow_Task_4();
+} else {flow_Task_5();
+};
+}
+
+public void task_generic_Task_4() {System.out.println("task_generic_Task 4");
+}
+
+public void task_generic_Task_5() {System.out.println("task_generic_Task 5");
+}
+
+public void task_generic_Task3() {System.out.println("task_generic_Task3");
+}
+
+public void task_generic_Task2() {System.out.println("task_generic_Task2");
+}
+
+public void task_generic_Task1() {System.out.println("task_generic_Task1");
+}}
