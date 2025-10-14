8cacd7161b6be85ad40afef30eb315cb9e786412
Giuseppe Della Penna <giuseppe.dellapenna@univaq.it>
2025-10-14 10:40:11 +0200

    Support default branch in inclusive gateway translation
    Added handling for default branches in BPMN inclusive gateways in both the BPMN diagram and the Java translator. The BPMN file now specifies a default flow and corresponding task, while the Java translator adds logic to select the default branch if no conditions are met.

--- src/main/java/dellapenna/personal/bpmn/bpmn/ToJavaBPMNTranslator.java
+++ src/main/java/dellapenna/personal/bpmn/bpmn/ToJavaBPMNTranslator.java
@@ -572,22 +572,37 @@ public class ToJavaBPMNTranslator extends AbstractBPMNTranslator<String> {
         Code code = new Code<String>(generateCommonNodeEntryStaments(g, info));
         code.append("java.util.List<java.util.function.Consumer<" + EXECUTILEXPRESSION + ".ProcessStatus>> enabledBranches = new java.util.ArrayList<>()");
         code.append("//CONDITIONALLY FORKS: " + splitFlows.stream().map(s -> s.firstStep().getId()).collect(Collectors.joining(",")));
-        for (int o = 0; o < splitFlows.size(); ++o) {
-            String condition_expression = splitFlows.get(o).condition().substring(1);
-            FeelTranslationInfo v_f_info = new FeelTranslationInfo();
-            code.append("if "
-                    + "(" + feel.translate(condition_expression, v_f_info) + ")" //TEMP, dobbiamo togliere l'uguale se c'è altrimenti non è un'espressione feel
-                    + "{");
-            code.append(generateTransitionDescriptionStaments(g, splitFlows.get(o).firstStep(), info));
-            code.append("enabledBranches.add(" + "this::" + sanitizeName(p.getFlowName(splitFlows.get(o).firstStep())) + ")");
-            code.append("} ");
-            p.registerProcessVariables(v_f_info.getUsedVariableNames(), BPMNDecodedProcess.VariableDirection.READ, g.getId(), condition_expression);
-            p.registerDecodedEdge(g, splitFlows.get(o).firstStep());
 
+        BPMNDecodedConditionalFlow default_branch = null;
+
+        for (int o = 0; o < splitFlows.size(); ++o) {
+            FeelTranslationInfo v_f_info = new FeelTranslationInfo();
+            if (splitFlows.get(o).condition() != null) {
+                String condition_expression = splitFlows.get(o).condition().substring(1);
+                code.append("if "
+                        + "(" + feel.translate(condition_expression, v_f_info) + ")" //TEMP, dobbiamo togliere l'uguale se c'è altrimenti non è un'espressione feel
+                        + "{"
+                        + generateCodeSource(generateTransitionDescriptionStaments(g, splitFlows.get(o).firstStep(), info))
+                        + "enabledBranches.add(" + "this::" + sanitizeName(p.getFlowName(splitFlows.get(o).firstStep())) + ");"
+                        + "} ");
+                p.registerProcessVariables(v_f_info.getUsedVariableNames(), BPMNDecodedProcess.VariableDirection.READ, g.getId(), condition_expression);
+                p.registerDecodedEdge(g, splitFlows.get(o).firstStep());
+
+            } else {
+                default_branch = splitFlows.get(o);
+            }
         }
+
+        code.append("if (enabledBranches.isEmpty()) "
+                + ((default_branch != null)
+                        ? "{ enabledBranches.add(" + "this::" + sanitizeName(p.getFlowName(default_branch.firstStep())) + "); }"
+                        : "{ " + EXECUTILEXPRESSION + ".noDefaultCaseError(s); }")
+        );
+
         code.append(EXECUTILEXPRESSION + ".fork(s,\"" + g.getId() + "\",enabledBranches.toArray(java.util.function.Consumer[]::new))");
         code.append(EXECUTILEXPRESSION + ".stopThread()");
         code.append(generateCommonNodeExitStatements(g, info));
+
         return code;
     }
 
